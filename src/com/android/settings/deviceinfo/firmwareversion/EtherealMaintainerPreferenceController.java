5a6b7920be47f64f64e6c04329399f40643dc474
IamCOD3X <sourabh.panchal28@gmail.com>
2024-01-19 00:36:15 +0530

    Path fix

--- src/com/android/settings/deviceinfo/firmwareversion/EtherealMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/EtherealMaintainerPreferenceController.java
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-package com.android.settings.deviceinfo.ethereal;
+package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
-import android.content.res.Resources;
+import android.os.SystemProperties;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -39,7 +39,8 @@ public class EtherealMaintainerPreferenceController extends BasePreferenceContro
     }
 
     public CharSequence getSummary() {
-        String maintainer = mContext.getResources().getString(R.string.ethereal_maintainer);
-        return maintainer;
+        String rom = SystemProperties.get(ROM_PROPERTY,
+                this.mContext.getString(R.string.device_info_default));
+        return rom;
     }
 }
